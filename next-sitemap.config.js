f2acba267e2384060dd9d9e81e43e414879219f4
SarmadSalee <sarmad.saleem62@gmail.com>
2026-06-27 12:53:52 +0500

    feat: add ads

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,16 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: "https://toolnova-delta.vercel.app",
-    generateRobotsTxt: true,
-};
+  siteUrl: 'https://toolnova-delta.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'weekly',
+  priority: 0.7,
+  sitemapSize: 5000,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+    ],
+  },
+}
